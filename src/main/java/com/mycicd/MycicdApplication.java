df219469347ebbffe5899eb9cfcd02e036292a70
Gaurav <gmanchanda144@gmail.com>
2026-01-10 16:31:13 +0530

    initial spring boot app

---
+++ src/main/java/com/mycicd/MycicdApplication.java
@@ -0,0 +1,13 @@
+package com.mycicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycicd/MycicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
