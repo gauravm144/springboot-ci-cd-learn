47ba8508325629cf985e7128d27e1a0084c38608
Gaurav <gmanchanda144@gmail.com>
2026-01-10 17:08:06 +0530

    initial spring boot app deploy yml changes

--- src/main/java/com/mycicd/Controller.java
+++ src/main/java/com/mycicd/Controller.java
@@ -1,12 +1,19 @@
 package com.mycicd;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class Controller {
-    @GetMapping("/hello")
-    public String home(){
-        return "Hello V2";
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to home";
+    }
+    
+    @GetMapping("/hello/{id}")
+    public String home(@PathVariable("id") String id){
+        return "Hello "+id;
     }
 }
