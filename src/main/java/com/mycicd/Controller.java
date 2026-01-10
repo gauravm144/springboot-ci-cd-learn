7ce18e645896dd90aba52f25c0ded428a9a029ee
Gaurav <gmanchanda144@gmail.com>
2026-01-10 16:39:09 +0530

    initial spring boot app v2

--- src/main/java/com/mycicd/Controller.java
+++ src/main/java/com/mycicd/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/hello")
     public String home(){
-        return "Hello V1";
+        return "Hello V2";
     }
 }
